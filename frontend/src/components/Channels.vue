c0984c067c793fb557484f82fba1b184f20b1fe7
mansur0987-del <85795437+mansur0987-del@users.noreply.github.com>
2023-07-11 15:33:50 +0400

    create and get all chats

--- frontend/src/components/Channels.vue
+++ frontend/src/components/Channels.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import WindowCreateChannel from './CreateChannel.vue'
+import axios from "axios";
 
 const emit = defineEmits<{
 	(e: 'GetChannelId', chennelId: number): void
 }>()
-
-async function GetChannelIdFromClick(clickChannelId: number) {
-	emit("GetChannelId", clickChannelId)
-}
 
 const WindowForCreateChannel = ref<number>(0);
 
@@ -16,30 +13,48 @@ async function CreateChannel() {
 	WindowForCreateChannel.value = 1;
 }
 async function EmitCloseWindow() {
+	channels.value = (await axios.get('chat/')).data
 	WindowForCreateChannel.value = 0
 }
+
+interface Channel {
+	id: number,
+	chat_name: string,
+	isPrivate: boolean,
+	have_password: boolean,
+	password: string
+}
+
+const channels = ref<Channel[]>()
+
+async function GetAllAccessChannels() {
+	channels.value = (await axios.get('chat/')).data
+}
+
+async function GetChannelIdFromClick(clickChannelId: number) {
+	emit("GetChannelId", clickChannelId)
+}
+
+onMounted(async () => {
+	await GetAllAccessChannels()
+})
 
 </script>
 
 <template>
 	<WindowCreateChannel v-if="WindowForCreateChannel" @CreateChannelWindowIsClose='EmitCloseWindow' />
 	<div class='Channels'>
-		<p>{{ WindowForCreateChannel }}</p>
 		<div class="buttonCreateChannel">
 			<button @click="CreateChannel()">
 				Create
 			</button>
 		</div>
 		<h1>Channels</h1>
-		<p @click="GetChannelIdFromClick(1)" style="cursor: pointer; color: blue">
-			Push 1
-		</p>
-		<p @click="GetChannelIdFromClick(2)" style="cursor: pointer; color: blue;">
-			Push 2
-		</p>
-		<p @click="GetChannelIdFromClick(3)" style="cursor: pointer; color: blue">
-			Push 3
-		</p>
+		<div v-for="channel in channels">
+			<p @click="GetChannelIdFromClick(channel.id)" style="cursor: pointer; color: blue">
+				{{ channel.chat_name }}
+			</p>
+		</div>
 	</div>
 </template>
 
