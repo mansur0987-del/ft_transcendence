f156a3933ac8159b4ee02e3647b36e44ac6594b1
mtsatryan <9057374+mtsatryan@users.noreply.github.com>
2023-07-12 17:23:12 +0400

    update

---
+++ frontend/src/components/Game/Pong.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="w-full md:w-4/5 h-screen bg-black">
+            <Multiplayer :gameSocket="gameSocket" :id="id" :mode="mode" />
+        </div>
+        <div class="w-full md:w-1/5 bg-gray-900 h-screen text-white">
+            Chat
+        </div>
+    </div>
+</template>
+
+<script>
+import Multiplayer from './Multiplayer';
+
+export default {
+    components: {
+        Multiplayer,
+    },
+    props: {
+        gameSocket: Object,
+        id: Number,
+        mode: Number,
+    },
+};
+</script>
