d825856478016786d3fdea195f0436064e51878a
Ruslan Andreev <rbell@c2r9s2.42yerevan.am>
2023-07-16 21:37:56 +0400

    msgTOChat + connect to chat

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -6,6 +6,7 @@ import {
 	OnGatewayConnection,
 	OnGatewayDisconnect,
 	MessageBody,
+	ConnectedSocket,
 } from '@nestjs/websockets';
 import { Logger } from '@nestjs/common';
 import { Socket, Server } from 'socket.io';
@@ -20,7 +21,7 @@ import {
 	Request,
 	UseGuards
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtService } from '@nestjs/jwt';
 
 @WebSocketGateway({
 	cors: {
@@ -28,20 +29,53 @@ import { AuthGuard } from '@nestjs/passport';
 	},
 	namespace: 'chat'
 })
-export class ChatGateway {//implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-	// @WebSocketServer()
-	// server: Server;
+export class ChatGateway {
+	constructor(
+		private readonly jwtService: JwtService
+	) { }
+	@WebSocketServer()
+	server: Server;
+	clients: Map<number, Socket[]>
+	//utils
+	async getTockenFromClient(client: Socket): Promise<string> {
+		if (!client || !client.handshake || !client.handshake.headers)
+			return null;
+		const token = client.handshake.headers.authorization && client.handshake.headers.authorization.split(' ')[1];
+		return token;
+	}
 
-	// private logger: Logger = new Logger('chatGateway');
+	async initSrv() {
+		if (!this.server) {
+			this.server = new Server();
+		}
+	}
 
+	async checkAuth(client: Socket): Promise<boolean> {
+		const token: string = await this.getTockenFromClient(client);
+		if (!token)
+			return (false);
+		try {
+			const user = await this.jwtService.verify(token, { publicKey: process.env.JWT_ACCESS_KEY });
+			console.log('user:\n', user);
+			return true;
+		}
+		catch { return false; }
+	}
 
+	//events
 	@SubscribeMessage('msgToServer')
-	handleMessage(client: Socket, @MessageBody() body: any): void {
-		console.log('\n\n\nbody msg', body, '\n\n\n');
-		console.log('\n\n\nclient socket:\n', client);
-		// client.on('sendMsgClient', (res) => {
-		// 	console.log(res);
-		// })
+	async handleMessage(@ConnectedSocket() client: Socket, @MessageBody() body: any): Promise<any> {
+		//check auth
+		if (!await this.checkAuth(client)) {
+			console.log('AUTH FAILED');
+			return await client.disconnect();
+		}
+		//check body
+		if (!body || !body.chat_id || !body.message)
+			throw new BadRequestException('have no body or body is invalid')
+		//check have permissions
+		//add message to database
+		//send msg to other
 	}
 
 
@@ -53,7 +87,21 @@ export class ChatGateway {//implements OnGatewayInit, OnGatewayConnection, OnGat
 	// 	this.logger.log(`Client disconnected: ${client.id}`);
 	// }
 
-	// handleConnection(client: Socket, ...args: any[]) {
-	// 	this.logger.log(`Client connected: ${client.id}`);
-	// }
+	@SubscribeMessage('connectToChat')
+	async connectToChat(client: Socket, @MessageBody() body: any) {
+		//check auth
+		if (!await this.checkAuth(client)) {
+			console.log('AUTH FAILED');
+			return await client.disconnect();
+		}
+		//init server
+		await this.initSrv();
+		//check body
+		if (!body || !body.chat_id)
+			throw new BadRequestException('have no body or body is invalid');
+		if (!await this.clients.has(body.chat_id))
+			await this.clients.set(body.chat_id, []);
+		await this.clients.get(body.chat_id).push(client);
+		console.log('client has connected');
+	}
 }
