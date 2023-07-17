2fa6ccb4e9f4c2fbb3ab6b929a1a53da270d73bd
Mampacuk <aleqsandr.i.s@tumo.org>
2023-07-18 02:37:58 +0400

    Multiplayer.vue started

---
+++ frontend/src/components/Multiplayer.vue
@@ -0,0 +1,30 @@
+<script>
+import {paper, Path, view, PointText } from 'paper';
+import { useRoute, useRouter } from 'vue_router';
+
+export default {
+	props: {
+		gameSocket: Object, // not sure about the type
+		mode: Number,
+		isInvited: Boolean,
+		playerId: Number,
+		toStart: Boolean,
+	},
+	setup(props) {
+		const canvasRef = ref(null);
+		const parentRef = ref(null);
+		const paddleWidth = 20 / 1080;
+		const paddleHeight = 200 / 1920;
+		var ballPosition = [0.5, 0.5];
+		var ballRadius = 20 / 1920;
+		var paddlePos = [[0, 0.5], [1 - paddleWidth, 0.5]];
+		var score = [0, 0];
+		const router = useRouter();
+
+		watchEffect(() => {
+
+		});
+	}
+}
+
+</script>
