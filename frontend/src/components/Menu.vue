429995d04eee78f314794f66b9d5f2f1f4d18213
mtsatryan <9057374+mtsatryan@users.noreply.github.com>
2023-07-20 02:37:54 +0400

    engine
    engine upd

--- frontend/src/components/Menu.vue
+++ frontend/src/components/Menu.vue
@@ -1,67 +1,59 @@
-<script>
+<script setup lang="ts">
 import Engine from './Engine.vue'
+import { Socket } from "socket.io-client";
+import { ref } from "vue";
 
-export default {
-    // need to get gameSocket and mode from the other components...
-    // Engine?
-    props: {
-        gameSocket: Socket, // not sure about the type
-        mode: number
-    },
-    setup(props) {
-        const isLoading = ref("Find Game");
-        const isHardcore = ref(false);
+const props = defineProps<{
+    gameSocket: Socket,
+    mode: number,
+}>()
+const isLoading = ref("Find Game");
+const isHardcore = ref(false);
 
-        const searchOpponent = () => {
-            try {
-                isLoading.value = "Searching...";
-                props.gameSocket.emit('add');
-            } catch (err) {
-                isLoading.value = "Error";
-                console.error(err);
-            }
-        }
-
-        const toggleMode = () => {
-            isHardcore.value = !isHardcore.value;
-            if (isHardcore.value)
-                mode = 0;
-            else
-                mode = 1;
-        }
-
-        return { searchOpponent, toggleMode, isLoading, isHardcore }
+const searchOpponent = () => {
+    try {
+        isLoading.value = "Searching...";
+        props.gameSocket.emit('add');
+    } catch (err) {
+        isLoading.value = "Error";
+        console.error(err);
     }
+}
+
+const toggleMode = () => {
+    isHardcore.value = !isHardcore.value;
+    if (isHardcore.value) { }
+    //  props.mode = 0; // WHY IS READONL
+    // else
+    //  props.mode = 1;
 }
 </script>
 
 <template>
-<div class='w-screen h-screen justify-center relative'> 
-          <div class='scene w-full h-full absolute'>
-              <Engine :isPreview="true" /> 
-          </div>
-        <div class='w-screen h-screen flex justify-center items-center'> 
-          <div class="w-1/2 h-fit flex flex-col justify-center items-center bg-gray-400 bg-opacity-70 rounded-lg p-8 relative z-10"> 
-            <h1 class="text-2xl font-bold text-center mb-4">Pong Game</h1>
-            <button
-                class="bg-gray-700 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded w-1/2 mb-4"
-                @click="searchOpponent()"
-            >
-           {{isLoading}}
-            </button>
+    <div class='w-screen h-screen justify-center relative'>
+        <div class='scene w-full h-full absolute'>
+            <Engine :isPreview="true" />
+        </div>
+        <div class='w-screen h-screen flex justify-center items-center'>
+            <div
+                class="w-1/2 h-fit flex flex-col justify-center items-center bg-gray-400 bg-opacity-70 rounded-lg p-8 relative z-10">
+                <h1 class="text-2xl font-bold text-center mb-4">Pong Game</h1>
+                <button class="bg-gray-700 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded w-1/2 mb-4"
+                    @click="searchOpponent()">
+                    {{ isLoading }}
+                </button>
 
-			<button
-                :class="isHardcore ? 'bg-red-700 hover:bg-red-950 text-white font-bold py-2 px-4 rounded w-1/2 mb-4' : 'bg-gray-700 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded w-1/2 mb-4'"
-                @Click="toggleMode()"
-            >
-                {{isHardcore ? "Hardcore" : "Standard"}}
-            </button>
-            <p class="text-center">Instructions:</p>
-            <ul class="list-disc list-inside mt-2">
-                <li>Use the W and S keys to move your paddle up and down</li>
-                <li>Use the space bar to pause the game</li>
-            </ul>
-          </div>
-          </div>
-      </div>
+                <button
+                    :class="isHardcore ? 'bg-red-700 hover:bg-red-950 text-white font-bold py-2 px-4 rounded w-1/2 mb-4' : 'bg-gray-700 hover:bg-gray-950 text-white font-bold py-2 px-4 rounded w-1/2 mb-4'"
+                    @Click="toggleMode()">
+                    {{ isHardcore ? "Hardcore" : "Standard" }}
+                </button>
+                <p class="text-center">Instructions:</p>
+                <ul class="list-disc list-inside mt-2">
+                    <li>Use the W and S keys to move your paddle up and down</li>
+                    <li>Use the space bar to pause the game</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
--- frontend/src/components/Pong.vue
+++ frontend/src/components/Pong.vue
@@ -1,26 +1,21 @@
+<script setup lang="ts">
+// import Multiplayer from "./Multiplayer.vue";
+import { Socket } from "socket.io-client";
+
+const props = defineProps<{
+    gameSocket: Socket,
+    mode: number,
+    playerId: number
+}>()
+</script>
+
 <template>
     <div>
         <div class="w-full md:w-4/5 h-screen bg-black">
-            <Multiplayer :gameSocket="gameSocket" :id="id" :mode="mode" />
+            <Multiplayer :gameSocket="gameSocket" :playerId="playerId" :mode="mode" />
         </div>
         <div class="w-full md:w-1/5 bg-gray-900 h-screen text-white">
             Chat
         </div>
     </div>
 </template>
-
-<script>
-//lang="ts"
-import Multiplayer from './Multiplayer.vue';
-
-export default {
-    components: {
-        Multiplayer,
-    },
-    props: {
-        gameSocket: Object,
-        id: Number,
-        mode: Number,
-    },
-};
-</script>
