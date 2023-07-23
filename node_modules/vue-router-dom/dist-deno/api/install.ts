c20f8dccd3cb650a94243d0e988f8b3106593d9f
mansur0987-del <85795437+mansur0987-del@users.noreply.github.com>
2023-07-24 00:14:53 +0400

    del

--- node_modules/vue-router-dom/dist-deno/api/install.ts
+++
@@ -1,9 +0,0 @@
-import * as components from '../components/index'
-
-import type { App } from 'vue'
-
-export const install = (app: App) => {
-  for (const key in components) {
-    app.component(key, components[key as keyof typeof components])
-  }
-}
