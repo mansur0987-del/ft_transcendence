92ccf434b521045435d52f5f8249e0189f9317aa
mansur0987-del <85795437+mansur0987-del@users.noreply.github.com>
2023-07-24 22:10:48 +0400

    send and get invite

--- frontend/src/components/LeftBar.vue
+++ frontend/src/components/LeftBar.vue
@@ -1,6 +1,43 @@
 <script setup lang="ts">
 import PlayerStatus from './PlayerStatus.vue'
 import { ElButton } from 'element-plus'
+import { Store } from "../pinia";
+import { onMounted, ref } from "vue";
+import type { Socket } from "socket.io-client";
+
+const store = Store()
+
+interface Invite {
+	name: string
+}
+let socketInvite: Socket
+const invites = ref<Invite[]>()
+onMounted(() => {
+	socketInvite = store.GetSocketInvite()
+
+	socketInvite.on('GetInvite', (data) => {
+		if (data.name) {
+			invites.value?.push({ name: data.name })
+		}
+	})
+})
+
+async function Cancel(name: string) {
+	socketInvite.emit('declinceInvite', { name: name })
+	await UpdateInvites(name)
+}
+
+async function Game(name: string) {
+	socketInvite.emit('acceptInvite', { name: name })
+	await UpdateInvites(name)
+}
+
+async function UpdateInvites(name: string) {
+	invites.value = invites.value?.filter((element) => {
+		return element.name !== name;
+	})
+}
+
 
 async function GetPlayer() {
 	window.location.assign('http://' + window.location.host + '/player')
@@ -49,6 +86,19 @@ async function GetChat() {
 			Chat
 		</el-button>
 	</div>
+	<div class="Invites">
+		<div v-for="invite in invites" style="margin-top: 5px; color: rgb(255, 255, 255); width: 200px;">
+			<p style="text-align: center;">
+				{{ invite.name }}
+			</p>
+			<p>
+				<el-button size="small" style="position: absolute; left: 0%;"
+					@click="Cancel(invite.name)">Cancel</el-button>
+				<el-button size="small" style="position: absolute; right: 0%;" @click="Game(invite.name)">Let's
+					play</el-button>
+			</p>
+		</div>
+	</div>
 </template>
 
 <style scoped>
@@ -77,5 +127,14 @@ async function GetChat() {
 	font-size: 16px;
 	border-radius: 10px;
 	height: 55px;
+}
+
+.Invites {
+	position: fixed;
+	right: 10%;
+	top: 1%;
+	height: fit-content;
+	max-height: 90%;
+	overflow: auto;
 }
 </style>
