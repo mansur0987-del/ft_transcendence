b943404095a4bca09704b377dba0097b48ec69c4
Ruslan Andreev <rbell@c2r11s1.42yerevan.am>
2023-07-29 17:48:05 +0400

    try query

--- backend/src/player/service/match.service.ts
+++ backend/src/player/service/match.service.ts
@@ -57,17 +57,19 @@ export class MatchService {
   async getStats(id: number): Promise<{rank: number, wins: number, loses: number}> {
     let allUsersStats = await this.matchRepo.query(`
       Select 
-	      COALESCE(t1.user_id, t2.user_id),
-	      COALESCE(t1.loses, 0) as loses,
-	      COALESCE(t2.wins, 0) as wins,
-	      COALESCE(t2.wins, 0) / (COALESCE(t2.wins, 0) + COALESCE(t2.loses, 0)) as wins_to_all
-        from (Select loser as user_id, count (distinct id) as loses
+       COALESCE(t1.user_id, t2.user_id),
+       COALESCE(t1.loses, 0) as loses,
+       COALESCE(t2.wins, 0) / (COALESCE(t2.wins, 0) + COALESCE(t1.loses, 0)) as wins_to_all
+       COALESCE(t2.wins, 0) as wins,
+        from (Select "loserId"  as user_id, count (distinct match_entity.id) as loses
         from match_entity
-        group by loser) as t1
-        full join where t1.user_id == t2.user_id
-        (Select winner as user_id, count (distinct id) as wins
+        group by "loserId") as t1
+        full join
+        (Select "winnerId" as user_id, count (distinct id) as wins
         from match_entity
-        group by winner) as t2
+        group by "winnerId") as t2
+        on t1.user_id = t2.user_id
+
     `)
     console.log(allUsersStats);
     return ({rank: 0, wins: 0, loses: 0});
